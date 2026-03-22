8bb73c3516dd27b1dac313a0cffab4e82e8fcb89
Noamm <116365534+Noamm9@users.noreply.github.com>
2026-03-22 16:08:26 +0200

    Update Nvgrenderer.kt

--- src/main/kotlin/com/github/noamm9/nvgrenderer/Nvgrenderer.kt
+++ src/main/kotlin/com/github/noamm9/nvgrenderer/Nvgrenderer.kt
@@ -15,40 +15,5 @@ import java.awt.Color
 class Nvgrenderer: ClientModInitializer {
     override fun onInitializeClient() {
         SpecialGuiElementRegistry.register { PIPNVG(it.vertexConsumers()) }
-
-        MultiPassBatcher.addCallback { ctx ->
-            ctx.push()
-            ctx.translate(200, 200)
-            ctx.scale(1.25)
-
-            ctx.nvg {
-                NVG.rect(0, 0, 120, 40, Color(0, 0, 0, 140), 6f)
-                NVG.text("NVG", 10, 10, 16, Color.WHITE, NVG.font)
-            }
-
-            ctx.gui { gfx: GuiGraphics ->
-                gfx.drawString(Minecraft.getInstance().font, "GuiGraphics", 0, 50, Color.WHITE.rgb)
-            }
-
-            ctx.pop()
-        }
-
-        MouseBatcher.addCallbackClick { event ->
-            if (event.button != 0) return@addCallbackClick false
-            val stack = MouseStack()
-            stack.push()
-            stack.translate(200, 200)
-            stack.scale(1.25)
-
-            if (stack.x.toInt() in 0 .. 120 && stack.y.toInt() in 0 .. 40) {
-                Minecraft.getInstance().gui.chat.addMessage(Component.literal("Clicked the nvg box"))
-                return@addCallbackClick true
-            }
-
-            stack.pop()
-            false
-        }
-
-        //  */
     }
 }
