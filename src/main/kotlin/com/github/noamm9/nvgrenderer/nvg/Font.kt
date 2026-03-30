22ef9b74798e3a99efbdc6d1ecbbff97f8f1a2a2
Noamm <116365534+Noamm9@users.noreply.github.com>
2026-03-31 01:10:41 +0300

    Change Font constructor visibility to public

--- src/main/kotlin/com/github/noamm9/nvgrenderer/nvg/Font.kt
+++ src/main/kotlin/com/github/noamm9/nvgrenderer/nvg/Font.kt
@@ -3,7 +3,7 @@ package com.github.noamm9.nvgrenderer.nvg
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
-class Font internal constructor(val location: String) {
+class Font(val location: String) {
     val buffer by lazy {
         val bytes = ResourceLoader.read(location)
         ByteBuffer.allocateDirect(bytes.size).order(ByteOrder.nativeOrder()).put(bytes).flip() as ByteBuffer
