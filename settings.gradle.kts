206371386c2e5d9b249d5d35adf3b87454fc78ce
DreamcastERA1 <danilsayahov05@gmail.com>
2026-05-24 21:31:34 +0300

    Added foojay resolver and (hopefully) fixed jitpack build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,7 @@
 pluginManagement {
+    plugins {
+        id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    }
     repositories {
         maven("https://maven.fabricmc.net/") {
             name = "Fabric"
